699e32cebd9e57cba00eccd68e3f98941adea580
wamal <wamal@SURFACEPRO4>
2023-02-23 17:28:32 +0100

    CoinDenomsOptions

---
+++ TerraSharp/Core/CoinDenomOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerraSharp.Core
+{
+    public class CoinDenomOptions : Attribute
+    {
+        public string ImageUrl { get; set; }
+        public bool MainnetSupport { get; set; }
+        public bool TestNetSupport { get; set; }
+        public double Decimals { get; set; }
+        public CoinDenomOptions(string imageUrl, bool mainnetSupport, bool testNetSupport, double decimals) 
+        {
+            ImageUrl= imageUrl;
+            MainnetSupport= mainnetSupport;
+            TestNetSupport= testNetSupport;
+            Decimals= decimals;
+        }
+    }
+}
