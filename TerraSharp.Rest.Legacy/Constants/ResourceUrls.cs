20c3ae8a49035186384a6c37403c892d787d2dbf
Dan Gerchcovich <dan.developer789@gmail.com>
2023-02-24 21:28:59 +1100

    Classic Update Migration

--- TerraSharp.Rest.Legacy/Constants/ResourceUrls.cs
+++ TerraSharp.Rest.Legacy/Constants/ResourceUrls.cs
@@ -6,7 +6,6 @@
     public class ResourceUrls
     {
         public const string LUNA2_TESTNET = "https://pisco-lcd.terra.dev";
-        public const string TERRA_REBELS_TESTNET = "https://lcd.terra.dev";
         public const string TERRA_LOCALTERRA_ROUTE = "http://localhost:1317";
         public const string LUNA2_PROD_ROUTE = "https://phoenix-lcd.terra.dev";
     }
